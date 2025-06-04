5ce720c1c7a10530479afcc83821d37bf73b46b9
Omsinzaza <natthananaew5@gmail.com>
2025-06-04 21:39:33 +0700

    Fix world crashing

--- src/main/java/net/sincere/jutsucraft/Jutsucraft.java
+++ src/main/java/net/sincere/jutsucraft/Jutsucraft.java
@@ -4,11 +4,13 @@ import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.sincere.jutsucraft.chakra.ChakraProvider;
 import net.sincere.jutsucraft.network.ModMessages;
 import org.slf4j.Logger;
@@ -17,6 +19,11 @@ import org.slf4j.Logger;
 public class Jutsucraft {
     public static final String MODID = "jutsucraft";
     private static final Logger LOGGER = LogUtils.getLogger();
+    public Jutsucraft() {
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modBus.addListener(this::commonSetup);
+        modBus.addListener(this::addCreative);
+    }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         ModMessages.register();
